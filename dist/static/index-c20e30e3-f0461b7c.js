a18d6409de4a94f0e5a409849a2bde5b999091a6
Julia Soria <soriajulias@gmail.com>
2023-04-26 19:23:55 +0200

    build

---
+++ dist/static/index-c20e30e3-f0461b7c.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-63f13ccc-091597c9.js";import"./sanity-6a0bd3a6.js";import"./index-74580352.js";export{m as DocumentPaneProvider,a as default};
--- schemas/schema.js
+++ schemas/schema.js
@@ -2,7 +2,7 @@ import institucion from "./institucion";
 import localidad from "./localidad";
 import novedad from "./novedad";
 import servicioTurnosOnline from "./servicioTurnosOnline";
-import faq from '/faq'
+import faq from './faq'
 
 export default [
     institucion,
